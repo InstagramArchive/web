2610a13e94a3dbd5a994312c14eedc071a8f88c3
Qypol342 <Qypol342@gmail.com>
2023-10-09 21:30:31 +0200

    feat(homepage): start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col w-screen h-screen">
+    <div class="flex overflow-hidden gap-3 items-center p-2 w-full h-16">
+        <div class="flex w-auto h-full">
+            <img class="w-full h-full rounded-xl" src="/logo.png" alt="logo">
+        </div>
+        <h1 class="text-[2.8rem]">Instagram Archive</h1>
+    </div>
+    <div class="flex w-full h-full">
+        <div class="flex justify-center items-center w-1/2 h-full">
+            <img class="max-w-full max-h-full" src="/explore.svg" alt="explore">
+        </div>
+        <div class="flex flex-col justify-center items-start w-1/2 h-full">
+            <h2 class="text-[3rem]">Explore the evolution of instagram account</h2>
+            <a class="p-2 text-white bg-pink-400 rounded-lg cursor-pointer">Explore 🔎</a>
+        </div>
+    </div>
+
+
+</div>
