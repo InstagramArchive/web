90a4c07b850481dcb08c2e4793fc8dab6bf70539
Dilini Liyanage <ldsliyanage99@gmail.com>
2023-10-12 15:51:21 +0530

    Build the Website Home Page #1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,41 @@
-<div class="flex flex-col w-screen h-screen">
-    <div class="flex overflow-hidden gap-3 items-center p-2 w-full h-16">
-        <div class="flex w-auto h-full">
-            <img class="w-full h-full rounded-xl" src="/logo.png" alt="logo">
-        </div>
-        <h1 class="text-[2.8rem]">Instagram Archive</h1>
-    </div>
-    <div class="flex w-full h-full">
-        <div class="flex justify-center items-center w-1/2 h-full">
-            <img class="max-w-full max-h-full" src="/explore.svg" alt="explore">
-        </div>
-        <div class="flex flex-col justify-center items-start w-1/2 h-full">
-            <h2 class="text-[3rem]">Explore the evolution of instagram account</h2>
-            <a class="p-2 text-white bg-pink-400 rounded-lg cursor-pointer">Explore 🔎</a>
-        </div>
-    </div>
+<div class="flex flex-col bg-cover h-screen" style="background-image: url('/homeBackground.png');">
+	<div class="flex overflow-hidden gap-3 items-center p-2 w-full h-16">
+		<div class="flex w-auto h-full">
+			<img class="w-full h-full rounded-xl m-2" src="/logo.png" alt="logo" />
+		</div>
+		<h1 class="text-[3rem] text-white">Instagram Archive</h1>
+		<div />
+	</div>
 
+	<div class="flex w-full h-full">
+		<div class="flex flex-col items-center justify-center w-full h-full">
+			<div class="flex flex-col text-white">
+				<p class="text-[70px]">
+					Use the <span class="text-[140px] inline">Wayback </span>
+				</p>
+				<p class="text-[80px]">Machine</p>
+			</div>
+			<div />
 
+			<div class="w-[25vh] h-[25vh] ml-[90vh] -mt-[10vh]">
+				<div class="relative group cursor-pointer">
+					<div
+						class="absolute -inset-1 bg-gradient-to-r from-gray-600 to-gray-400 rounded-3xl blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"
+					/>
+					<div
+						class="relative px-6 py-6 bg-white ring-1 ring-gray-900/5 rounded-3xl leading-none flex items-top justify-start space-x-5"
+					>
+						<div>
+							<p class="text-[40px] text-center font-bold">START</p>
+							<img
+								class="w-[10vh] h-[10vh] rounded-xl ml-5 "
+								src="/rightLogo.png"
+								alt="rightLogo"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
