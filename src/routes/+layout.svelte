eb72b40912ebb7d8908a2c7e2df326b8e629b1c5
qypol342 <qypol342@gmail.com>
2023-10-29 16:23:05 +0100

    feat(auth) init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
-<script>
-    import "../app.css";
-  </script>
+ <!-- src/routes/+layout.svelte -->
+<script lang="ts">
+	import '../styles.css'
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+  import "../app.css";
+
+
+	export let data
+
+	let { supabase, session } = data
+	$: ({ supabase, session } = data)
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
+</script>
+
+
   
   <slot />
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,22 @@
+<!-- src/routes/+page.svelte -->
+<script lang="ts">
+	import { Auth } from '@supabase/auth-ui-svelte'
+
+	export let data
+</script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="row flex-center flex">
+	<div class="col-6 form-widget">
+		<Auth
+			supabaseClient={data.supabase}
+			view="magic_link"
+			redirectTo={`${data.url}/auth/callback`}
+			showLinks={false}
+			appearance={{  style: { input: 'color: #fff' } }}
+		/>
+	</div>
+</div>
